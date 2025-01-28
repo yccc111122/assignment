839faa896f28eb58dfbd89b6b9fd7e5ef681f896
yccc111122 <142764706+yccc111122@users.noreply.github.com>
2025-01-28 14:39:39 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const outputDirectory = 'build'; // 输出目录设为 build 以配合 Firebase Hosting
+const outputDirectory = 'build'; 
 
 module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'], // 确保入口文件正确
+  entry: ['babel-polyfill', './src/client/index.js'], 
   output: {
-    path: path.join(__dirname, outputDirectory), // 输出目录
+    path: path.join(__dirname, outputDirectory), 
     filename: 'bundle.js'
   },
   module: {
@@ -44,7 +44,7 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanWebpackPlugin([outputDirectory]), // 旧版本 CleanWebpackPlugin 用法
+    new CleanWebpackPlugin([outputDirectory]), 
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
